089bf25cf875273f20fd020acc8d490794af910e
jonesus <joonas.palosuo@gmail.com>
2019-10-05 12:40:23 +0300

    Trying to fix styled jsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,14 @@
 import React from 'react';
 
-const Home = () => <h1>Mumbai TV</h1>;
+const Home: React.FC = () => (
+  <>
+    <h1>Mumbai TV</h1>
+    <style jsx>{`
+      h1 {
+        font-size: 3rem;
+      }
+    `}</style>
+  </>
+);
 
 export default Home;
